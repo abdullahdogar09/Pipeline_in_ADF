9c194fbd138ad875f4dd2a8f9f77553d0914ef9a
abdullahdogar09 <abdullah009dogar@gmail.com>
2025-12-08 11:07:28 +0500

    Modifies usp_CreateDimReceiver.sql

--- pipeline_in_ADF/SQL_Stored_Procedures/usp_CreateDimReceiver.sql
+++ pipeline_in_ADF/SQL_Stored_Procedures/usp_CreateDimReceiver.sql
@@ -1,4 +1,4 @@
-/****** Object:  StoredProcedure [dbo].[usp_CreateDimReceiver]    Script Date: 11/16/2025 5:29:59 PM ******/
+/****** Object:  StoredProcedure [dbo].[usp_CreateDimReceiver]    Script Date: 12/8/2025 11:03:51 AM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
